57f0a37739f2181e0fa85f0c7077462848013fd9
Andrew <aogrady@beca324.org>
2021-08-01 23:13:52 -0400

    changes to Financial

--- Teaching/Financial.js
+++ Teaching/Financial.js
@@ -1,23 +1,19 @@
-// JavaScript Document
-
-
-
 function investCalc1() {
 
-  var principal = document.getElementById("principal").value;
-  var rate = document.getElementById("rate").value;
-  var years = document.getElementById("years").value;
-  var growth = 1 +Number(rate);
+  var principal = document.getElementById("principal").valueAsNumber;
+  var rate = document.getElementById("rate").valueAsNumber;
+  var years = document.getElementById("years").valueAsNumber;
+  var growth = 1 + rate;
 
   //var result1=principal*(growth)**years;
-  
-  var  results1_str = "";
+
+  var results1_str = "";
 
   for (let counter = 0; counter < years; counter++) {
-    let result1 = principal*(growth)**counter;
-    console.log(typeof(result1))
+    let result1 = principal * ((growth) ** counter);
+    console.log(typeof (result1));
     result1 = result1.toFixed(2); //Rounds to 2 decimal places
-    results1_str += "Year: " + counter + "   Amount:  " + String(result1) + "<br>" 
+    results1_str += "Year: " + counter + "   Amount:  " + String(result1) + "<br>"
     console.log(result1);
     document.getElementById("resultCalc1").innerHTML = results1_str; //prints to the div
   }
@@ -42,35 +38,21 @@ function showHideCalc2() {
   }
 }
 
-// var slider = document.getElementById("rate_slider");
-// var output = document.getElementById("demo");
-
-// output.innerHTML = slider.value; // Display the default slider value
-// // Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function() {
-//   output.innerHTML = this.value;
-// }
-
 function investCalc2() {
 
   var principal2 = document.getElementById("principal2").valueAsNumber;
   var rate2 = document.getElementById("rate2").valueAsNumber;
   var years2 = document.getElementById("years2").valueAsNumber;
   var additional2 = document.getElementById("additional2").valueAsNumber;
-  var growth2 = 1 + Number(rate2);
+  var growth2 = 1 + rate2;
 
-  //var result1=principal*(growth)**years;
-  
- 
-  let result2 = principal2;
+  var result2 = principal2;
   var results2_str = "Year 0: Amount: " + String(result2) + "<br>"
 
-  for (let counter = 1; counter < (years2+1); counter++) {
-    result2 = principal2*(growth2)**counter + Number(additional2);
+  for (let counter = 1; counter < (years2 + 1); counter++) {
+    result2 = result2 * growth2 + additional2;
     result2 = result2.toFixed(2); //Rounds to 2 decimal places
-    results2_str += "Year: " + counter + "   Amount:  " + String(result2) + "<br>" 
-    document.getElementById("resultCalc2").innerHTML = results2_str; 
+    results2_str += "Year: " + counter + "   Amount:  " + String(result2) + "<br>"
+    document.getElementById("resultCalc2").innerHTML = results2_str;
   }
 }
-
-
