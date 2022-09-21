2718fb688e5e4c29588d09f76fdd56b4dcd1e9d8
drogrady <88150647+drogrady@users.noreply.github.com>
2022-09-21 10:12:26 -0400

    cleaned up files

--- The Music Store/main.js
+++
@@ -1,8 +0,0 @@
-﻿const salesTaxRate = .075;
-incMsg = "This site is under construction and will be fully functional soon.";
-function product(item, description, price, imgsrc) {
-    this.item = item;
-    this.description = description;
-    this.price = price;
-    this.imgsrc = imgsrc;
-}
