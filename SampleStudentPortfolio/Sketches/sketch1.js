e4005a3152a77c239b0038ac756470996a8e342a
drogrady <88150647+drogrady@users.noreply.github.com>
2023-05-25 08:22:22 -0400

    Add files via upload

--- SampleStudentPortfolio/Sketches/sketch1.js
+++ SampleStudentPortfolio/Sketches/sketch1.js
@@ -1,22 +1,29 @@
 function setup() {
-  createCanvas(400, 400); // Create a canvas to draw on
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220); // Set the background color
+  background(220);
   
-  // Set face color and draw a circle for the face
-  fill(255); 
-  stroke(0);
-  circle(200, 200, 200);
+//Draw Head
+  fill('lemonchiffon');
+   ellipse(200,200,300,300);
   
-  // Set eye color and draw two circles for the eyes
-  fill(0);
-  circle(150, 170, 40);
-  circle(250, 170, 40);
+//Add Eyes
+  strokeWeight(5);
+  fill(0,0,255)
+  circle(130,130,40);
   
-  // Set mouth color and draw an arc for the mouth
-  noFill();
-  strokeWeight(3);
-  arc(200, 230, 80, 50, 0, PI);
+  
+  //Draw Nosey
+  fill('white')
+  triangle(210,185,176,228, 231,227)
+  
+//Show Mouse coordinates
+  textSize(20)
+  strokeWeight(1)
+  fill(0,0,0);
+  text('Mouse X:  '+ mouseX,0,20);
+  text('Mouse Y:  '+ mouseY,0,40);
+  
 }
